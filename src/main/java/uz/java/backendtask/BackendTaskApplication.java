6dd53176bc92806b90ed4d406b20213f3885c585
Saidafzalkhon <saidafzalxon.dev@gmail.com>
2026-01-10 21:22:36 +0500

    Initial commit

---
+++ src/main/java/uz/java/backendtask/BackendTaskApplication.java
@@ -0,0 +1,13 @@
+package uz.java.backendtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTaskApplication.class, args);
+    }
+
+}
